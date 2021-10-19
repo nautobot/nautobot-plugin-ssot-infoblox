5b8e22eb2be737a2602815b44eb13157fe8c8d99
Stephen Kiely <stephenkiely87@gmail.com>
2021-10-19 17:36:32 -0500

    Start work on Adapters and Models.

--- nautobot_ssot_infoblox/diffsync/adapters.py
+++ nautobot_ssot_infoblox/diffsync/adapters.py
@@ -1,1 +1,12 @@
 """Adapters for Infoblox integration with SSoT plugin."""
+
+from diffsync import DiffSync
+from .models import NautobotPrefix
+
+class NautobotAdapter(DiffSync):
+
+    prefix = NautobotPrefix
+
+    top_level = ("prefix")
+
+    def __init__
