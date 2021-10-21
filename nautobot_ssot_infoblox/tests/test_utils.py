ccf4ae4aefeaf2db2f5fb7260dd885f230b1d3ae
jtdub <amascuba@gmail.com>
2021-10-21 15:07:33 +0000

    linting

--- nautobot_ssot_infoblox/tests/test_utils.py
+++ nautobot_ssot_infoblox/tests/test_utils.py
@@ -1,18 +1,20 @@
 """Util tests that do not require Django."""
 import unittest
-import os
 
 from nautobot_ssot_infoblox.utils import get_vlan_view_name, nautobot_vlan_status
+
 
 class TestUtils(unittest.TestCase):
     """Test Utils."""
 
-    def test_vlan_view_name(self):
+    def test_vlan_view_name(self):  # pylint: disable=no-self-use
         """Test vlan_view_name util."""
-        name = get_vlan_view_name(resource="vlan/ZG5zLnZsYW4kLmNvbS5pbmZvYmxveC5kbnMudmxhbl92aWV3JFZMVmlldzEuMTAuMjAuMTA:VLView1/VL10/10")
+        name = get_vlan_view_name(
+            "vlan/ZG5zLnZsYW4kLmNvbS5pbmZvYmxveC5kbnMudmxhbl92aWV3JFZMVmlldzEuMTAuMjAuMTA:VLView1/VL10/10"
+        )
         assert name == "VLView1"
-    
-    def nautobot_vlan_status(self):
+
+    def nautobot_vlan_status(self):  # pylint: disable=no-self-use
         """Test nautobot_vlan_status."""
         status = nautobot_vlan_status("Active")
         assert status == "ASSIGNED"
