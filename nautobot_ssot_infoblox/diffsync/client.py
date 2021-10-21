b2ea0ca022981b8a444ec73a6399c3c2a7d2c62d
jtdub <amascuba@gmail.com>
2021-10-21 01:32:49 +0000

    black

--- nautobot_ssot_infoblox/diffsync/client.py
+++ nautobot_ssot_infoblox/diffsync/client.py
@@ -741,10 +741,10 @@ class InfobloxApi:  # pylint: disable=too-many-public-methods,  too-many-instanc
 
     def get_vlans(self):
         """Retrieve all VLANs from Infoblox.
-        
+
         Returns:
-            List: list of dictionaries  
-        
+            List: list of dictionaries
+
         Return Response:
         [
             {
@@ -773,18 +773,16 @@ class InfobloxApi:  # pylint: disable=too-many-public-methods,  too-many-instanc
             vlan_id (Int): VLAN ID (1-4094)
             vlan_name (Str): VLAN name
             parent_id (Str): The _ref ID of the parent VlanView in Infoblox
-        
+
         Returns:
-            Str: _ref to created vlan 
-        
+            Str: _ref to created vlan
+
         Return Response:
         "vlan/ZG5zLnZsYW4kLmNvbS5pbmZvYmxveC5kbnMudmxhbl92aWV3JFZMVmlldzEuMTAuMjAuMTE:VLView1/test11/11"
         """
         url_path = "vlan"
         params = {}
-        payload = {"parent": parent_id,
-                    "id": vlan_id,
-                    "name": vlan_name}
+        payload = {"parent": parent_id, "id": vlan_id, "name": vlan_name}
         response = self._request("POST", url_path, params=params, json=payload)
         logger.info(response.json)
         return response.json()
