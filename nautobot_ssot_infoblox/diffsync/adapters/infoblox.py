7c5a653040f4db4f5eabb36279e069e7d8856425
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2022-09-15 07:55:20 -0500

    test: :rotating_light: Remove unused import for flake8

--- nautobot_ssot_infoblox/diffsync/adapters/infoblox.py
+++ nautobot_ssot_infoblox/diffsync/adapters/infoblox.py
@@ -5,7 +5,6 @@ import re
 from diffsync import DiffSync
 from diffsync.enum import DiffSyncFlags
 from nautobot.extras.plugins.exceptions import PluginImproperlyConfigured
-from nautobot_ssot_infoblox.utils.client import InfobloxApi
 from nautobot_ssot_infoblox.utils.diffsync import get_ext_attr_dict, build_vlan_map
 from nautobot_ssot_infoblox.diffsync.models.infoblox import (
     InfobloxAggregate,
